f7e40616baa8aea8f379f1ba4ba8b4a7b111946a
Ariel Marinho <167530405+arieldreis@users.noreply.github.com>
2025-05-05 10:03:08 -0300

    I create new files

--- hashtag_programacao/hashtah_books_ariel/src/App.jsx
+++ hashtag_programacao/hashtah_books_ariel/src/App.jsx
@@ -10,10 +10,20 @@ function App() {
     capa: brasCubasImg,
     textoAlternativo: "Capa do Livro Memórias Póstumas de Brás Cubas.",
   };
+  // Logo abaixo dentro do return estará todos os elementos html
   return (
     <>
-      <Capa imagemCapa = {informacoesLivro.capa} textoAlternativo = {informacoesLivro.textoAlternativo}/>
+      <Capa 
+        imagemCapa = {informacoesLivro.capa} 
+        textoAlternativo = {informacoesLivro.textoAlternativo}
+      />
     </>
-  )
+  );
 }
 export default App
+/*
+"Props" é abreviação de "properties" (propriedades).
+Pensa assim: você tem um componente que é tipo uma função, e você quer que ele receba uma informação de fora. 
+Em vez de digitar essa info direto dentro dele, você passa via props, igual faz com parâmetros em funções no JavaScript.
+*/
+// mpm run dev = abrir o link do terminal
--- hashtag_programacao/hashtah_books_ariel/src/Capa.jsx
+++ hashtag_programacao/hashtah_books_ariel/src/Capa.jsx
@@ -1,5 +1,6 @@
-function Capa(){
-    return <h1>Mémorias Póstumas</h1>
+function Capa(props){
+    return <img className="capa" src={props.imagemCapa} alt={props.textoAlternativo} />
 }
 
 export default Capa;
+// O attibuto className é mesma coisa que o class do html é diferença é que o className só usado no REACT.
