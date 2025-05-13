d37e67dfa003e26d4d7edbd20d0015af95679fbc
Ariel Marinho <167530405+arieldreis@users.noreply.github.com>
2025-05-13 09:35:26 -0300

    Add comands.

--- Spotify/replica_do_spotify/vite.config.js
+++ Spotify/replica_do_spotify/vite.config.js
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  root: '.',                   // já tá na pasta front_end
-  base: '/',                   // rota base pros assets
-  publicDir: path.resolve(__dirname, 'public'), // se usar public/
-  build: {
-    outDir: path.resolve(__dirname, '../dist'), // build vai pra /dist na raiz
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   plugins: [react()],
 })
-
