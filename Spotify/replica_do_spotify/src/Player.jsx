cbf1afe4a398529f2eec9608b027fdbe4cf412ac
Ariel Marinho <167530405+arieldreis@users.noreply.github.com>
2025-05-14 16:38:45 -0300

    Time in play

--- Spotify/replica_do_spotify/src/Player.jsx
+++ Spotify/replica_do_spotify/src/Player.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"; // Importar um ícone
 import {faCirclePlay, faCirclePause,faBackwardStep, faForwardStep} from "@fortawesome/free-solid-svg-icons"; // ImportAR O TIPO DE ÍCONE
 import { Link } from 'react-router-dom';
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { useState } from 'react';
 const formatTime = (timeInSeconds) => {
     const minutes = Math.floor(timeInSeconds / 60).toString().padStart(2, "0");
@@ -16,8 +16,14 @@ const Player = ({duration, randomIdFromArtist, randomId2FromArtist, audio}) => {
     const playPause = () => {
         isPlaying ? audioPlayer.current.pause() : audioPlayer.current.play();
         setIsPlaying(!isPlaying);
-        setCurrentTime(formatTime(audioPlayer.current.currentTime));
     }
+    // Codigo do tempo da música
+    useEffect(() => {
+        const intervaId = setInterval(() => {
+            setCurrentTime(formatTime(audioPlayer.current.currentTime));
+        }, 1000);
+        return () => clearInterval(intervaId);
+    }, [isPlaying]);
   return (
     <div className='player'>
         <div className="player__controllers">
