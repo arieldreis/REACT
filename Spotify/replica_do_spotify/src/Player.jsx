cf237204cd35c0e88117c80431a506962995f3e2
Ariel Marinho <167530405+arieldreis@users.noreply.github.com>
2025-05-14 23:10:13 -0300

    Fishish the Spotify Project.

--- Spotify/replica_do_spotify/src/Player.jsx
+++ Spotify/replica_do_spotify/src/Player.jsx
@@ -9,10 +9,21 @@ const formatTime = (timeInSeconds) => {
     const seconds = Math.floor(timeInSeconds - minutes * 60).toString().padStart(2, "0");
     return `${minutes}:${seconds}`
 };
+
+const timeInSeconds = (timeString) => {
+    const splitArray = timeString.split(':');
+    const minutes = Number(splitArray[0]);
+    const seconds = Number(splitArray[1]);
+
+    return seconds + minutes * 60
+};
+
 const Player = ({duration, randomIdFromArtist, randomId2FromArtist, audio}) => {
     const audioPlayer = useRef();
+    const progressBar = useRef();
     const [isPlaying, setIsPlaying] = useState(false);
     const [currentTime, setCurrentTime] = useState(formatTime(0));
+    const durationInSeconds = timeInSeconds(duration);
     const playPause = () => {
         isPlaying ? audioPlayer.current.pause() : audioPlayer.current.play();
         setIsPlaying(!isPlaying);
@@ -21,6 +32,8 @@ const Player = ({duration, randomIdFromArtist, randomId2FromArtist, audio}) => {
     useEffect(() => {
         const intervaId = setInterval(() => {
             setCurrentTime(formatTime(audioPlayer.current.currentTime));
+            progressBar.current.style.setProperty('--_progress', 
+                ((audioPlayer.current.currentTime / durationInSeconds) * 100) + "%");
         }, 1000);
         return () => clearInterval(intervaId);
     }, [isPlaying]);
@@ -42,7 +55,7 @@ const Player = ({duration, randomIdFromArtist, randomId2FromArtist, audio}) => {
         <div className="player__progress">
             <p>{currentTime}</p>
             <div className="player__bar">
-                <div className="player__bar-progress"></div>
+                <div className="player__bar-progress" ref={progressBar}></div>
             </div>
             <p>{duration}</p>
         </div>
